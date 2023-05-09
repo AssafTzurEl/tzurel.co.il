320382688482d488e6a3af648da3cadac619630c
Assaf Tzur-El <28855903+AssafTzurEl@users.noreply.github.com>
2023-05-09 18:44:51 +0300

    Remove Algolia configuration

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -28,25 +28,4 @@ module.exports = {
     //socialYouTube: "UCH60RRaY2GI9m62z1loLjcA",
     // Google Analytics ID  (optional, remove it not needed), used for... well, Google Analytics
     googleAnalytics: "G-C0JG54YQ03",
-    // Algolia-powered search  (optional, remove it not needed),
-    // See: https://github.com/algolia/algoliasearch-netlify
-    // algoliaSearch: {
-    //     // When enabled shows the search bar in the UI
-    //     enabled: true,
-    //     // You'll have to set this manually in your build settings.
-    //     // The value comes from Algolia, and is either visible in the
-    //     // UI for the Crawler Plugin or the Algolia Dashboard.
-    //     appId: process.env.ALGOLIA_APP_ID,
-    //     // You'll have to set this manually in your build settings.
-    //     // The value comes from Algolia, and is either visible in the
-    //     // UI for the Crawler Plugin or the Algolia Dashboard.
-    //     searchApiKey: process.env.ALGOLIA_SEARCH_API_KEY,
-    //     // You'll have to set this manually in your build settings.
-    //     // The value comes from Algolia, and is either visible in the
-    //     // UI for the Crawler Plugin or the Algolia Dashboard.
-    //     siteId: process.env.ALGOLIA_SITE_ID,
-    //     // Assuming that you deploy your "main" branch. Otherwise you
-    //     // can either override or configure this (using process.env.HEAD)
-    //     branch: "main"
-    // }
 };
